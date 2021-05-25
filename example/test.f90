def630a2f2298c9ce26afc04e683ab9b5845fcc1
zoziha <1325686572@qq.com>
2021-05-26 01:00:27 +0800

    daily

---
+++ example/test.f90
@@ -0,0 +1,15 @@
+!! fpm test test
+program main
+    use forlab
+    implicit none
+    real(RPRE) :: x(2, 2)
+
+    x = 0.0
+    x(1, 1) = 1.0
+    x(1, 2) = 2.0
+    x(2, 1) = 3.0
+    x(2, 2) = 4.0
+
+    call disp(x, 'x matrix:')
+
+end program
