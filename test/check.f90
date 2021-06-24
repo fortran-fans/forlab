f7f036861fb01afc75985cf72d84bdcff1cd7be7
zoziha13 <1325686572@qq.com>
2021-06-25 01:10:43 +0800

    modify: 1. num2str add cmplx support 2. disp improve cmplx condition note: test passed for stdlib: check.f90🎉

--- test/check.f90
+++ test/check.f90
@@ -1,19 +1,47 @@
 !! fpm test test
 program main
-    block
+
+    
+    block   !! forlab_num2str.fypp
         use forlab, only: setcolor, disp, num2str
         use iso_fortran_env
             call setcolor(2)
             !!! CMPLX
+            call disp('num2str(c): ')
             call disp(num2str((1,1))) 
             call disp(num2str((1,1), '(F6.2)'))
             !!! INT
+            call disp('num2str(i): ')
             call disp(num2str(2))
-            call disp(num2str(2), '(I6)')
+            call disp(num2str(2, '(I6)'))
             !!! REAL
+            call disp('num2str(r): ')
             call disp(num2str((1.0))) 
             call disp(num2str((1.0), '(F6.2)'))
 
     endblock
+
+
+    block   !! forlab_disp.fypp
+        use forlab, only: setcolor, disp, num2str, ones
+        use iso_fortran_env
+        complex :: c(2,2)
+        real :: r(2,2)
+        integer :: i(2,2)
+        call ones(c)
+        call ones(r)
+        call ones(i)
+
+            call setcolor(2)
+            !!! CMPLX
+            call disp(c, 'ones(c): ') 
+            !!! INT
+            call disp(i, 'ones(i): ')
+            !!! REAL
+            call disp(r, 'ones(r): ') 
+
+    endblock
+
+
 
 end program
