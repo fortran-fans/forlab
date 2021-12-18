c9df9e4b4c3df72c0e30e4ec07898971888c380b
zoziha <zuo.zhihua@qq.com>
2021-12-18 13:13:12 +0000

    Deploying to forlab-fpm from @ fortran-fans/forlab@13b78efac27b090f0fa77c43bd6023dc489d3166 🚀

---
+++ src/forlab_io_read_line.f90
@@ -0,0 +1,64 @@
+submodule (forlab_io) forlab_io_read_line
+
+    use, intrinsic :: iso_fortran_env, only: stdin => input_unit
+    implicit none
+    character(*), parameter :: nl = new_line("\n")
+    
+contains
+
+    !> Read a line from the input unit.
+    subroutine read_line(line, unit, iostat)
+    
+        character(:), allocatable, intent(out) :: line
+        integer, intent(in), optional :: unit
+        integer, intent(out), optional :: iostat
+        
+        integer :: unit_, iostat_
+        character(len=512) :: line_
+        character(len=512) :: msg
+        
+        unit_ = optval(unit, stdin)
+        
+        line = ""
+        read(unit_, "(A512)", iostat=iostat_, iomsg=msg) line_
+        if (present(iostat)) then
+            iostat = iostat_
+            if (iostat_ == 0) line = trim(line_)
+        else
+            if (iostat_ == 0) then
+                line = trim(line_)
+            else
+                error stop msg
+            end if
+        end if        
+    
+    end subroutine read_line
+    
+    !> Read ASCII file as a string.
+    subroutine read_file(string, file, iostat, keep_newline)
+        
+        character(:), allocatable, intent(out) :: string
+        character(*), intent(in) :: file
+        integer, intent(out), optional :: iostat
+        logical, intent(in), optional :: keep_newline
+        
+        integer :: iostat_, unit, count, i
+        character(:), allocatable :: string_
+        character(len=512) :: msg
+        logical :: keep_newline_
+        
+        keep_newline_ = optval(keep_newline, .true.)
+        open(newunit=unit, file=file)
+        
+        string = ""
+        count = countlines(file)
+        do i = 1, count
+            call read_line(string_, unit, iostat_)
+            if (keep_newline_) string_ = string_ // nl
+            string = string//string_
+        end do
+        close(unit)
+        
+    end subroutine read_file
+
+end submodule forlab_io_read_line
